68837e0c056db06a51fbba6c2b98067b31f69900
Akash22-cyber <nathakana03@gmail.com>
2026-09-16 00:40:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,19 @@
-// Global array to store customer objects
-let customers = [];
+function validateCode() {
 
-function addCustomer() {
+    // Get product code from input field
+    let productCode = document.getElementById("productCode").value;
 
-    // Get values from input fields
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let age = document.getElementById("age").value;
-    let height = document.getElementById("height").value;
-    let weight = document.getElementById("weight").value;
+    // Regular expression:
+    // # followed by exactly 4 uppercase letters
+    // followed by exactly 3 digits
+    let pattern = /^#[A-Z]{4}[0-9]{3}$/;
 
-    // Create customer object
-    let customer = {
-        name: name,
-        email: email,
-        age: age,
-        height: height,
-        weight: weight
-    };
-
-    // Add object to global array
-    customers.push(customer);
-
-    // Display customers
-    displayCustomers();
-
-    // Clear input fields
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("age").value = "";
-    document.getElementById("height").value = "";
-    document.getElementById("weight").value = "";
-}
-
-function displayCustomers() {
-
-    let table = `
-        <table border="1">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Age</th>
-                <th>Height</th>
-                <th>Weight</th>
-            </tr>
-    `;
-
-    // Loop through customer array
-    for (let customer of customers) {
-        table += `
-            <tr>
-                <td>${customer.name}</td>
-                <td>${customer.email}</td>
-                <td>${customer.age}</td>
-                <td>${customer.height}</td>
-                <td>${customer.weight}</td>
-            </tr>
-        `;
+    // Check product code
+    if (pattern.test(productCode)) {
+        document.getElementById("result").innerHTML =
+            "Product code verified successfully";
+    } else {
+        document.getElementById("result").innerHTML =
+            "Product code is not valid";
     }
-
-    table += `</table>`;
-
-    // Display table in webpage
-    document.getElementById("customerTable").innerHTML = table;
 }
