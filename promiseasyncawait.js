e610fab9943b330513490c61493a335dc6f960b4
Akash22-cyber <nathakana03@gmail.com>
2026-09-21 18:24:55 +0530

    Create promiseasyncawait.js

---
+++ promiseasyncawait.js
@@ -0,0 +1,34 @@
+function fetchStudent(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (id > 0) {
+                resolve({ id: id, name: "John Doe" });
+            } else {
+                reject("Invalid Student ID");
+            }
+        }, 2000);
+    });
+}
+
+fetchStudent(1)
+    .then(student => {
+        console.log(student);
+        return student.name;
+    })
+    .then(name => {
+        console.log(name);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+async function getStudent(id) {
+    try {
+        const student = await fetchStudent(id);
+        console.log(student);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getStudent(2);
